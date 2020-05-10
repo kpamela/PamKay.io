58ed086a9f229abc902f254f24ddfb2c90e0ebd7
Pam <khouripamela@gmail.com>
2020-05-10 15:38:56 -0400

    Adjusted grid layout for consistency on web

--- pamkayio/src/views/Homepage.js
+++ pamkayio/src/views/Homepage.js
@@ -2,20 +2,21 @@ import React from 'react';
 import './style.css';
 import Typewriter from 'typewriter-effect';
 import pamIcon from '../images/pam.jpg';
-import GridContainer from "../components/Grid/GridContainer.js";
-import GridItem from "../components/Grid/GridItem.js";
+import Button from "../components/Button/Button.js";
+import Grid from '@material-ui/core/Grid';
+
+
 
 function HomePage() {
   return (
     <div className="HomePage">
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={6}>
+      <Grid container spacing={3}>
+        <Grid xs={6}>
           <div className = "image-container">
             <img className = "image" src={pamIcon}/>
           </div>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={6}>
-          <div className="HomePage-header">
+        </Grid>
+        <Grid xs={6}>
             <p className= "HomePage-title">
               Hey! <br />I'm Pam
             </p>
@@ -23,8 +24,8 @@ function HomePage() {
              I do{'\u00A0'}
               <Typewriter
                 options={{
-                  strings: ['<span style="color:#B0CCBD";> UX / UI design </span>',
-                  '<span style="color:#A7F893;"> web development </span>',
+                  strings: ['<span style="color:#6AA65B";> UX / UI design </span>',
+                  '<span style="color:#37BC16;"> web development </span>',
                   '<span style=";color:#9A6FE4;"> photography </span>',
                   '<span style="display:inline;color:#636BD9;"> videography </span>' ],
                   autoStart: true,
@@ -36,12 +37,16 @@ function HomePage() {
               />
             </div>
             <div className="HomePage-regular-text">
-            I'm building one hell of a story (this site is under construction) but you can visit the following links meanwhile.
+              I'm building one hell of a story (this site is under construction) but you can visit the following links meanwhile. <br/>
             </div>
-          </div>
-
-        </GridItem>
-      </GridContainer>
+            <div className="HomePage-buttons-container">
+              <a href="https://www.linkedin.com/in/pamela-khouri"> <Button size="md" skeleton>  Linkedin </Button></a>
+              <a href="https://www.pamkay.ca">  <Button size="md" skeleton>  Portfolio </Button></a>
+              <a href="https://www.github.com/kpamela">  <Button size="md" skeleton>  Github </Button></a>
+              <a href="https://www.instagram.com/pamkay.ca">  <Button size="md" skeleton>  Instagram </Button></a>
+            </div>
+        </Grid>
+      </Grid>
 
     </div>
   );
