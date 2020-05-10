b0cf0cdd3e7fbef4ebbfd7df5e7f9422a12143c4
Pam <khouripamela@gmail.com>
2020-05-10 02:18:05 -0400

    Homepage design added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './views/Homepage.js';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- my-app/src/views/Homepage.js
+++ my-app/src/views/Homepage.js
@@ -3,37 +3,34 @@ import './style.css';
 import Typewriter from 'typewriter-effect';
 import pamIcon from '../images/pam.jpg';
 
+
 function HomePage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <p className= "HomePage-title">
+          Hey! <br />I'm Pam
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <img className="ui medium circular image" src='../images/pam.jpg'/>
-        I am
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter.typeString('Hello World!')
-              .callFunction(() => {
-                console.log('String typed out!');
-              })
-              .pauseFor(2500)
-              .deleteAll()
-              .callFunction(() => {
-                console.log('All strings were deleted');
-              })
-              .start();
-          }}
-        />
+        <div className= "HomePage-typewriter">
+        <img src={pamIcon} />
+        I do{'\u00A0'}
+          <Typewriter
+            options={{
+              strings: ['<span style="color:#B0CCBD";> UX / UI design </span>',
+              '<span style="color:#A7F893;"> web development </span>',
+              '<span style=";color:#9A6FE4;"> photography </span>',
+              '<span style="display:inline;color:#636BD9;"> videography </span>' ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 20,
+              delay: 55,
+              pauseFor: 50
+            }}
+          />
+        </div>
+        <div className="HomePage-regular-text">
+        I'm building one hell of a story (this site is under construction) but you can visit the following links meanwhile.
+        </div>
       </header>
     </div>
   );
